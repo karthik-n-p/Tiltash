7246e07d1a2da1fa71b48e23921f6bf31032a184
Thushar Vinod V <thushar3013@gmail.com>
2025-04-19 14:34:44 +0530

    generated ssl certificate for frontend https

--- test-project/vite.config.js
+++ test-project/vite.config.js
@@ -1,25 +1,15 @@
-
-import { defineConfig } from 'vite'
-
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
-
- plugins: [react()],
-
- server: {
-
-  host: true, // allows external access
-
-  strictPort: true,
-
-  port: 5173,
-
-  allowedHosts: ['.ngrok-free.app'], // allows all ngrok subdomains
-
- },
-
-})
-
-
-
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem')
+    },
+    port: 3000,
+    host: true
+  }
+});
